7f0bf9706fc6337ba51bd62775fd1ae728618762
Ayodeji-spec456 <aayodeji010@gmail.com>
2025-10-19 15:47:26 +0100

    update cors

--- Backend/app.js
+++ Backend/app.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(
   cors({
-    origin: ["http://localhost:3000"], // Allow frontend dev server and production
+    origin: ["http://localhost:3000", "https://medical-xvdx-git-master-ayodejis-projects-59ca9000.vercel.app"], // Allow frontend dev server and production
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
