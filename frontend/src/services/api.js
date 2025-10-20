b981df07846e17cc658dbf0a96f0cbd7868b41c4
Ayodeji-spec456 <aayodeji010@gmail.com>
2025-10-20 13:00:10 +0100

    medical

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_BASE_URL } from '../utils/constants';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || API_BASE_URL,
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
