748c7a3e80a8076bd518282bed9730f430b952e7
Ayodeji-spec456 <aayodeji010@gmail.com>
2025-10-20 12:43:37 +0100

    medical

--- Backend/app.js
+++ Backend/app.js
@@ -17,7 +17,9 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(cors());
 
-app.use(cors({ origin: "*", credentials: true }));
+app.use(
+  cors({ origin: "https://medical-xvdx.vercel.app/", credentials: true })
+);
 // app.use(
 //   cors({
 //     origin: [
