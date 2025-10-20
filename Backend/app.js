3943bc695cb3bdd6c80a92e5353603282409eb66
Ayodeji-spec456 <aayodeji010@gmail.com>
2025-10-20 12:32:37 +0100

    medical

--- Backend/app.js
+++ Backend/app.js
@@ -15,13 +15,19 @@ const PORT = process.env.PORT || 5000;
 
 // 🔐 Security and Middleware
 app.use(helmet());
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://medical-xvdx-git-master-ayodejis-projects-59ca9000.vercel.app"], // Allow frontend dev server and production
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
+
+app.use(cors({ origin: "*", credentials: true }));
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000",
+//       "https://medical-xvdx-git-master-ayodejis-projects-59ca9000.vercel.app/",
+//     ], // Allow frontend dev server and production
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
 app.use(morgan("dev"));
 app.use(express.json({ limit: "10mb" }));
 
